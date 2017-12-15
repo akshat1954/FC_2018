974c629482915281b2a33487ef181901a0a16b9d
aksha <aksha@DESKTOP-KO4DE1P>
2017-12-16 02:27:38 +0530

    fran 16-12-2017

--- FranConnect/src/franconnect/FcSky_17166_Sales.java
+++ FranConnect/src/franconnect/FcSky_17166_Sales.java
@@ -1,3 +1,4 @@
+package franconnect;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
@@ -26,7 +27,7 @@ public class FcSky_17166_Sales {
 	  
 	  @BeforeTest
 	  public void beforeTest() {
-	   System.setProperty("webdriver.chrome.driver", "D:\\Automation\\2018_Automation\\chromedriver_win32\\chromedriver.exe");
+	   System.setProperty("webdriver.chrome.driver", "D:\\Franconnect_2018\\chromedriver_win32\\chromedriver.exe");
 	   driver = new ChromeDriver();
 	   baseUrl = "https://clusterdc.franconnect.net/fc/";
 	   driver.manage().window().maximize();
